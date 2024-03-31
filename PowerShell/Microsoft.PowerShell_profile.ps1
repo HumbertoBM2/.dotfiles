5c67b95cae32808576f7f6144550c95a93c0a409
Humberto Barrera <117108901+HumbertoBM2@users.noreply.github.com>
2024-03-31 16:32:07 -0600

    Added dotfiles

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,52 @@
+cls
+#Set-PSReadLineOption -PredictionViewStyle ListView
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+function prompt {
+    $path = Get-Location
+    $gitBranch = ""
+    $gitStatus = ""
+
+    # Check if inside a Git repo
+    $gitRepo = Get-Command git -ErrorAction SilentlyContinue
+    if ($gitRepo -ne $null -and (Test-Path .git)) {
+        $branchName = (git rev-parse --abbrev-ref HEAD)
+        $gitBranch = " git:($($branchName))"
+
+        # Check if there are changes in the Git repo
+        $gitStatus = (git status --porcelain)
+        if ($gitStatus) {
+            $gitStatus = " ✗"
+        }
+    }
+
+    # Customize colors and formatting
+    $arrow = " "
+    $boldText = "`e[1m"
+    $resetFormatting = "`e[0m"
+
+    # Extract current directory from the full path and make it bold
+    $currentDirectory = "${boldText}$((Get-Item $path).Name)${resetFormatting}"
+
+    # Construct the prompt string
+    $promptString = "${arrow}${currentDirectory}${gitBranch}${gitStatus} "
+    $promptString
+}
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+
+
+
+
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {"lua","arduino","asm","bash","c","c_sharp","cpp","css","gitignore","html","java","javascript","json","latex","matlab","markdown","python","r","sql","toml","typescript","verilog","yaml"},
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
