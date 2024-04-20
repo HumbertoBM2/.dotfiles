c1a10d236f918b57f3ea3bed8b73174066a653a5
Humberto Barrera <117108901+HumbertoBM2@users.noreply.github.com>
2024-04-19 20:03:35 -0600

    Update md-preview.lua

--- nvim/lua/plugins/md-preview.lua
+++ nvim/lua/plugins/md-preview.lua
@@ -3,9 +3,4 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
-    config = function()
-        require('markdown-preview').setup({
-            options = {}
-        })
-    end
 }
