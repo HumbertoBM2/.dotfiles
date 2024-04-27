1350361167e431c2feb4f948360175d794cbda61
Humberto Barrera <117108901+HumbertoBM2@users.noreply.github.com>
2024-04-27 15:38:58 -0600

    Updated nvim  config

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,4 +15,3 @@ vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("lazy").setup("plugins")
 vim.cmd("language en_US")
-vim.g.moonflyItalics = false
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,66 +1,49 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require('lualine').setup({
-      options = {
-        section_separators = "",
-        component_separators = "│",
-        globalstatus = true,
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = {
-          "branch",
-          {
-            "diff",
-            symbols = {
-              added = " ",
-              modified = " ",
-              removed = " ",
-            },
-          },
-          {
-            "diagnostics",
-            symbols = {
-              error = " ",
-              warn = " ",
-              info = " ",
-              hint = " ",
-            },
-          },
+    [1] = "nvim-lualine/lualine.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {
+        options = {
+            section_separators = "",
+            component_separators = "│",
+            globalstatus = true,
         },
-        lualine_c = {
-          {
-            "filetype",
-            icon_only = true,
-            padding = {
-              left = 1,
-              right = 0,
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = {
+                "branch",
+                "diff",
+                "diagnostics",
             },
-            separator = {
-              right = "",
+            lualine_c = {
+                {
+                    "filetype",
+                    icon_only = true,
+                    padding = {
+                        left = 1,
+                        right = 0,
+                    },
+                    separator = {
+                        right = "",
+                    },
+                },
+                "filename",
             },
-          },
-          "filename",
+            lualine_x = {
+                function()
+                    local count
+                    if (vim.fn.mode()):lower() == "v" then
+                        count = vim.fn.wordcount().visual_words
+                    else
+                        count = vim.fn.wordcount().words
+                    end
+                    return ("%sW"):format(count)
+                end,
+            },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
         },
-        lualine_x = {
-          function()
-            local count
-            if (vim.fn.mode()):lower() == "v" then
-              count = vim.fn.wordcount().visual_words
-            else
-              count = vim.fn.wordcount().words
-            end
-            return ("%sW"):format(count)
-          end,
+        extensions = {
+            "lazy",
         },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      extensions = {
-        "lazy",
-      },
-    })
-  end
+    },
 }
-
