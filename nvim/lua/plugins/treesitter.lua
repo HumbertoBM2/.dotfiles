8891b99e3a6cb8ca835132ec475e41f6a13a4508
Humberto Barrera <117108901+HumbertoBM2@users.noreply.github.com>
2024-05-02 09:07:07 -0600

    Updated nvim config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,8 +4,11 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua","arduino","asm","bash","c","c_sharp","cpp","css","gitignore","html","java","javascript","json","latex","matlab","markdown","python","r","sql","toml","typescript","verilog","yaml","bibtex","go","gdscript"},
-      highlight = { enable = true },
+      ensure_installed = {"lua","arduino","asm","bash","c","c_sharp","cpp","css","gitignore","html","java","javascript","json","matlab","latex","markdown","python","r","sql","toml","typescript","verilog","yaml","bibtex","go","gdscript"},
+      highlight = {
+        enable = true,
+        disable = { "latex" },
+      },
       indent = { enable = true },
     })
   end
